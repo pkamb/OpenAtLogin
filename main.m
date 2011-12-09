ba9f47eac6e0f274c2b21c741305acbaaa8b67c1
Peter Kamb <pkamb@pkamb-MBP-OSX.local>
2011-12-09 00:13:05 -0800

    Revert "Deleted all items related to the sample project"
    This reverts commit 23d1865617a81624ef9edda62145dc8fdd40b2c5.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SGLaunchAtLogin
+//
+//  Created by Justin Williams on 3/1/08.
+//  Copyright Second Gear LLC 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
