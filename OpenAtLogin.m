4e61fea2af1f2b18f673a2102e2cc504af0c9eec
Peter Kamb <citizenkamb@gmail.com>
2013-04-22 12:58:03 -0700

    - Updated for ARC

--- OpenAtLogin.m
+++ OpenAtLogin.m
@@ -30,7 +30,7 @@
 @implementation OpenAtLogin (PrivateMethods)
 - (void)enableLoginItemWithLoginItemsReference:(LSSharedFileListRef )theLoginItemsRefs ForPath:(NSString *)appPath {
 	// We call LSSharedFileListInsertItemURL to insert the item at the bottom of Login Items list.
-	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath];
+	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
 	LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(theLoginItemsRefs, kLSSharedFileListItemLast, NULL, NULL, url, NULL, NULL);		
 	if (item)
 		CFRelease(item);
@@ -42,10 +42,10 @@
 	// We're going to grab the contents of the shared file list (LSSharedFileListItemRef objects)
 	// and pop it in an array so we can iterate through it to find our item.
 	CFArrayRef loginItemsArray = LSSharedFileListCopySnapshot(theLoginItemsRefs, &seedValue);
-	for (id item in (NSArray *)loginItemsArray) {		
-		LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)item;
+	for (id item in (__bridge NSArray *)loginItemsArray) {		
+		LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)item;
 		if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &thePath, NULL) == noErr) {
-			if ([[(NSURL *)thePath path] hasPrefix:appPath]) {
+			if ([[(__bridge NSURL *)thePath path] hasPrefix:appPath]) {
 				LSSharedFileListItemRemove(theLoginItemsRefs, itemRef); // Deleting the item
 			}
 			// Docs for LSSharedFileListItemResolve say we're responsible
@@ -64,10 +64,10 @@
 	// We're going to grab the contents of the shared file list (LSSharedFileListItemRef objects)
 	// and pop it in an array so we can iterate through it to find our item.
 	CFArrayRef loginItemsArray = LSSharedFileListCopySnapshot(theLoginItemsRefs, &seedValue);
-	for (id item in (NSArray *)loginItemsArray) {    
-		LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)item;
+	for (id item in (__bridge NSArray *)loginItemsArray) {
+		LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)item;
 		if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &thePath, NULL) == noErr) {
-			if ([[(NSURL *)thePath path] hasPrefix:appPath]) {
+			if ([[(__bridge NSURL *)thePath path] hasPrefix:appPath]) {
 				found = YES;
 				break;
 			}
@@ -107,10 +107,6 @@
 			[self disableLoginItemWithLoginItemsReference:loginItems ForPath:appPath];
 	}
 	CFRelease(loginItems);
-}
-
-- (void)dealloc {
-	[super dealloc];
 }
 
 @end
